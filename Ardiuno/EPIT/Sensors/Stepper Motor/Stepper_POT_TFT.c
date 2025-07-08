3dcb584fd4650662b3558d62569d2ccbbf6c918a
komaldasarirao8499 <komaldasarirao8499@gmail.com>
2025-07-08 18:32:43 +0530

    Changed

--- Ardiuno/EPIT/Sensors/Stepper Motor/Stepper_POT_TFT.c
+++ Ardiuno/EPIT/Sensors/Stepper Motor/Stepper_POT_TFT.c
@@ -2,164 +2,214 @@
 #include <Adafruit_ST7789.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <TimerOne.h>
 
 #define TFT_CS 53
 #define TFT_RST 8
 #define TFT_DC 7
 #define TFT_BL 9
+
+#define STEP_PIN 12
+#define DIR_PIN 11
+#define EN_PIN 10
 #define POT_PIN A8
 #define BUTTON_PIN 46
-#define DIR_PIN 11
-#define STEP_PIN 12
-#define EN_PIN 10
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
-bool direction = true;
-bool motorEnabled = true;
-bool lastButtonState = HIGH;
-unsigned long buttonPressTime = 0;
-bool buttonHandled = false;
+volatile bool stepState = false;
+bool motorRunning = false;
+bool directionClockwise = true;
 
-int potValue = 0;
-unsigned long stepDelay = 1000;
-unsigned long lastStepTime = 0;
-bool stepState = LOW;
+bool buttonPressed = false;
+unsigned long pressStartTime = 0;
+int holdSeconds = 0;
+int lastReportedHold = -1;
 
-unsigned long lastDisplayUpdate = 0;
-
-String lastDirectionText = "";
-int lastSpeedShown = -1;
-int lastBars = -1;
-bool lastMotorStatus = true;
+int lastSpeed = -1;
+bool lastMotorState = false;
+String lastDirection = "";
 
 void setup() {
-  Wire.begin();
+  Serial.begin(9600);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(EN_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+  digitalWrite(EN_PIN, HIGH);
+  digitalWrite(DIR_PIN, directionClockwise ? HIGH : LOW);
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
   tft.init(240, 320);
   tft.setRotation(1);
-
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(EN_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-  digitalWrite(EN_PIN, LOW);
-  digitalWrite(DIR_PIN, direction);
-
-  drawLayout();
-  updateDisplay(true);
+  tft.fillScreen(ST77XX_BLACK);
+  drawStaticLayout();
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(stepperISR);
 }
 
 void loop() {
-  potValue = analogRead(POT_PIN);
-  stepDelay = map(potValue, 0, 1023, 2000, 200);
-  unsigned long now = micros();
+  handleButton();
+  updateStepperSpeed();
+  updateTFT();
+  delay(50);
+}
 
-  bool buttonState = digitalRead(BUTTON_PIN);
-  if (lastButtonState == HIGH && buttonState == LOW) {
-    buttonPressTime = millis();
-    buttonHandled = false;
-  }
-  if (lastButtonState == LOW && buttonState == HIGH) {
-    unsigned long pressDuration = millis() - buttonPressTime;
-    if (!buttonHandled) {
-      if (pressDuration > 700) {
-        motorEnabled = !motorEnabled;
-        digitalWrite(EN_PIN, motorEnabled ? LOW : HIGH);
-      } else {
-        direction = !direction;
-        digitalWrite(DIR_PIN, direction);
-      }
-      buttonHandled = true;
-      updateDisplay(true);
-    }
-  }
-  lastButtonState = buttonState;
-
-  if (motorEnabled && potValue >= 10 && potValue <= 1020) {
-    if (now - lastStepTime >= stepDelay) {
-      lastStepTime = now;
-      stepState = !stepState;
-      digitalWrite(STEP_PIN, stepState);
-    }
+void stepperISR() {
+  if (motorRunning) {
+    digitalWrite(STEP_PIN, stepState);
+    stepState = !stepState;
   } else {
     digitalWrite(STEP_PIN, LOW);
   }
+}
 
-  if (millis() - lastDisplayUpdate > 150) {
-    lastDisplayUpdate = millis();
-    updateDisplay(false);
+void updateStepperSpeed() {
+  int potVal = analogRead(POT_PIN);
+  int speed = map(potVal, 0, 1023, 2000, 200);
+  Timer1.setPeriod(speed);
+}
+
+void handleButton() {
+  bool state = digitalRead(BUTTON_PIN);
+
+  if (state == HIGH && !buttonPressed) {
+    buttonPressed = true;
+    pressStartTime = millis();
+    holdSeconds = 0;
+    lastReportedHold = -1;
+  }
+
+  if (buttonPressed && state == HIGH) {
+    int currentHold = (millis() - pressStartTime) / 1000;
+    if (currentHold != lastReportedHold) {
+      lastReportedHold = currentHold;
+    }
+  }
+
+  if (buttonPressed && state == LOW) {
+    int duration = (millis() - pressStartTime) / 1000;
+
+    if (duration >= 5) {
+      directionClockwise = !directionClockwise;
+      digitalWrite(DIR_PIN, directionClockwise ? HIGH : LOW);
+      Serial.println("Direction toggled");
+    } else if (duration >= 2) {
+      motorRunning = !motorRunning;
+      digitalWrite(EN_PIN, motorRunning ? LOW : HIGH);
+      Serial.println("Motor toggled");
+    }
+
+    buttonPressed = false;
+    holdSeconds = 0;
+    lastReportedHold = -1;
   }
 }
 
-void drawLayout() {
+void drawStaticLayout() {
   tft.fillScreen(ST77XX_BLACK);
-  tft.fillRect(10, 10, 310, 35, ST77XX_YELLOW);
-  tft.setCursor(20, 18);
-  tft.setTextColor(ST77XX_BLACK);
+  tft.fillRect(10, 10, 300, 50, ST77XX_YELLOW);
+  tft.setCursor(20, 17);
+  tft.setTextColor(ST77XX_RED);
   tft.setTextSize(3);
-  tft.print("Stepper Driver");
-
-  tft.drawRect(10, 50, 310, 30, ST77XX_WHITE);
-  tft.setCursor(14, 58);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(1);
-  tft.print("Short: CW/ACW | Long: ON/OFF");
-
-  tft.setCursor(15, 98);
+  tft.print("Stepper Motor");
+  tft.fillRect(10, 70, 300, 35, ST77XX_BLUE);
+  tft.setCursor(20, 80);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(2);
+  tft.print("2sec = ON/OFF 5sec = DIR");
+  tft.fillRect(10, 110, 300, 35, ST77XX_GREEN);
+  tft.setCursor(20, 120);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setTextSize(2);
+  tft.print("Holding:");
+  tft.fillRect(10, 150, 300, 35, ST77XX_WHITE);
+  tft.setCursor(20, 160);
+  tft.setTextColor(ST77XX_BLACK);
+  tft.setTextSize(2);
   tft.print("Direction:");
-
-  tft.setCursor(15, 138);
-  tft.print("Speed:");
+  tft.drawRect(10, 190, 300, 50, ST77XX_WHITE);
 }
 
-void updateDisplay(bool full) {
-  String currentDirection = direction ? "Clockwise" : "AntiClock";
-  int currentSpeed = motorEnabled ? map(potValue, 0, 1023, 0, 100) : 0;
-  int bars = (motorEnabled && potValue <= 1020) ? map(potValue, 0, 1023, 0, 27) : 0;
+void updateTFT() {
+  static int lastSpeed = -1;
+  static bool lastRunning = false;
+  static int lastHold = -1;
+  static String lastDir = "";
+  static String lastStatus = "";
 
-  if (full || currentDirection != lastDirectionText) {
-    tft.fillRect(150, 90, 160, 30, ST77XX_BLACK);
-    tft.setCursor(155, 98);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.setTextSize(2);
-    tft.print(currentDirection);
-    lastDirectionText = currentDirection;
-  }
-
-  if (full || currentSpeed != lastSpeedShown || motorEnabled != lastMotorStatus) {
-    tft.fillRect(150, 130, 160, 30, ST77XX_BLACK);
-    tft.setCursor(165, 138);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.setTextSize(2);
-    if (!motorEnabled)
-      tft.print("OFF");
-    else
-      tft.print(currentSpeed);
-    lastSpeedShown = currentSpeed;
-  }
-
-  if (full || bars != lastBars || motorEnabled != lastMotorStatus) {
-    int xStart = 10, yBase = 234, barWidth = 9, barSpacing = 2, height = 58;
-    for (int i = 0; i < 27; i++) {
-      int x = xStart + i * (barWidth + barSpacing);
-      if (i < bars) {
-        uint16_t color = ST77XX_GREEN;
-        if (i >= 5)  color = ST77XX_YELLOW;
-        if (i >= 15) color = ST77XX_ORANGE;
-        if (i >= 22) color = ST77XX_RED;
-        tft.fillRect(x, yBase - height, barWidth, height, color);
-      } else {
-        tft.fillRect(x, yBase - height, barWidth, height, ST77XX_BLACK);
-        tft.drawRect(x, yBase - height, barWidth, height, ST77XX_WHITE);
-      }
+  if (lastHold != lastReportedHold) {
+    tft.fillRect(150, 118, 140, 20, ST77XX_GREEN);
+    tft.setCursor(160, 120);
+    tft.setTextColor(ST77XX_BLACK);
+    tft.setTextSize(3);
+    if (buttonPressed) {
+      tft.print(lastReportedHold);
+      tft.print("s   ");
+    } else {
+      tft.print("-     ");
     }
-    lastBars = bars;
+    lastHold = lastReportedHold;
   }
 
-  lastMotorStatus = motorEnabled;
+  String dir = directionClockwise ? "Clockwise" : "AntiClockwise";
+  if (dir != lastDir) {
+    tft.fillRect(130, 155, 170, 25, ST77XX_WHITE);
+    tft.setCursor(140, 160);
+    tft.setTextColor(ST77XX_RED);
+    tft.setTextSize(2);
+    tft.print(dir);
+    lastDir = dir;
+  }
+
+  int barWidth = 300;
+  int barHeight = 40;
+  int barX = 10;
+  int barY = 195;
+  int potValue = analogRead(POT_PIN);
+  int mappedSpeed = map(potValue, 0, 1023, 0, 255);
+
+  if (motorRunning) {
+    if (mappedSpeed > 240 && lastStatus != "max") {
+      tft.fillRect(barX, barY, barWidth, barHeight, ST77XX_BLACK);
+      tft.setCursor(60, 205);
+      tft.setTextColor(ST77XX_RED);
+      tft.setTextSize(2);
+      tft.print("Max Limit Reached");
+      lastStatus = "max";
+    } else if (mappedSpeed <= 30 && lastStatus != "low") {
+      tft.fillRect(barX, barY, barWidth, barHeight, ST77XX_BLACK);
+      tft.setCursor(50, 205);
+      tft.setTextColor(ST77XX_YELLOW);
+      tft.setTextSize(2);
+      tft.print("Low Level Reached");
+      lastStatus = "low";
+    } else if (mappedSpeed > 30 && mappedSpeed <= 240) {
+      int fillW = map(potValue, 0, 1023, 0, barWidth);
+
+      for (int x = 0; x < barWidth; x++) {
+        float ratio = (float)x / barWidth;
+        uint8_t r = ratio * 255;
+        uint8_t g = 255 - r;
+        uint8_t b = (1 - abs(0.5 - ratio) * 2) * 255;
+        uint16_t color = tft.color565(r, g, b);
+        for (int y = 0; y < barHeight; y++) {
+          if (x < fillW)
+            tft.drawPixel(barX + x, barY + y, color);
+          else
+            tft.drawPixel(barX + x, barY + y, ST77XX_BLACK);
+        }
+      }
+      lastStatus = "normal";
+    }
+  } else if (lastRunning != motorRunning) {
+    tft.fillRect(10, 190, 300, 50, ST77XX_BLACK);
+    tft.setCursor(90, 200);
+    tft.setTextColor(ST77XX_RED);
+    tft.setTextSize(2);
+    tft.print("MOTOR OFF");
+    lastStatus = "off";
+  }
+
+  lastRunning = motorRunning;
 }
